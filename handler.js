9d93d8b8a73404563189acd87d73043c1e283819
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-06-18 22:10:09 -0700

    first

---
+++ handler.js
@@ -0,0 +1,62 @@
+const crypto = require('crypto');
+const fetch = require('unfetch');
+
+function signRequestBody(key, body) {
+  return `sha1=${crypto.createHmac('sha1', key).update(body, 'utf-8').digest('hex')}`;
+}
+
+module.exports.githubWebhookListener = (event, context, callback) => {
+  var errMsg; // eslint-disable-line
+  const token = process.env.GITHUB_WEBHOOK_SECRET;
+  const netlifyWebhook = process.env.NETLIFY_WEBHOOK_BUILD;
+  const headers = event.headers;
+  const sig = headers['X-Hub-Signature'];
+  const githubEvent = headers['X-GitHub-Event'];
+  const id = headers['X-GitHub-Delivery'];
+  const calculatedSig = signRequestBody(token, JSON.stringify(event.body));
+
+  if (typeof token !== 'string') {
+    errMsg = '[401] must provide a \'GITHUB_WEBHOOK_SECRET\' env variable';
+    return callback(new Error(errMsg));
+  }
+
+  if (!sig) {
+    errMsg = '[401] No X-Hub-Signature found on request';
+    return callback(new Error(errMsg));
+  }
+
+  if (!githubEvent) {
+    errMsg = '[422] No X-Github-Event found on request';
+    return callback(new Error(errMsg));
+  }
+
+  if (!id) {
+    errMsg = '[401] No X-Github-Delivery found on request';
+    return callback(new Error(errMsg));
+  }
+
+  if (sig !== calculatedSig) {
+    errMsg = '[401] X-Hub-Signature incorrect. Github webhook token doesn\'t match';
+    return callback(new Error(errMsg));
+  }
+
+  /* eslint-disable */
+  console.log('---------------------------------');
+  console.log(`Github-Event: "${githubEvent}" with action: "${event.body.action}"`);
+  console.log('---------------------------------');
+  console.log('Payload', event.body);
+  /* eslint-enable */
+
+  // Do custom stuff here with github event data
+  // For more on events see https://developer.github.com/v3/activity/events/types/
+  fetch(netlifyWebhook, {method: 'POST'}, body: JSON.stringify({}));
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      input: event,
+    }),
+  };
+
+  return callback(null, response);
+};
